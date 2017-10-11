5ac72e99c9d7aa4b7366d33d1da7103dba99c7aa
Dae In Chung <Dane@NMB.local>
2017-10-10 20:34:50 -0400

    2017 loops

---
+++ 2017/loops/js/script.js
@@ -0,0 +1,22 @@
+var students = [
+  {
+    name: "eleni",
+    file1: "",
+    file2: ""
+  },
+  {
+    name: "mazzy",
+    file1: "",
+    file2: ""
+  },
+  {
+    name: "brandon",
+    file1: "",
+    file2: ""
+  },
+  {
+    
+  }
+  
+  
+];
